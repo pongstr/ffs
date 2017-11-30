fbc8007dd7b3c912ba85d761fb96302fcacb7e11
Pongstr <twiz.tickler@gmail.com>
2017-11-30 16:51:31 +0200

    updates

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-const functions  = require('firebase-functions');
+const functions = require('firebase-functions');
 const express = require('./lib/express');
 
 exports.tools = functions.https.onRequest(express);
